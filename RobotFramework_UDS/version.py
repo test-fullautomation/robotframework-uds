bf6a1ceee9680214310f3a3e1a64229b82d669fa
ntd1hc <ngoan.tranduy@vn.bosch.com>
2024-10-17 13:59:43 +0700

    update version information

--- RobotFramework_UDS/version.py
+++ RobotFramework_UDS/version.py
@@ -12,5 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-VERSION      = '0.1.5'
-VERSION_DATE = '10.10.2024'
+VERSION      = '0.1.6'
+VERSION_DATE = '17.10.2024'
