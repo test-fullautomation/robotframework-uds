1f081e0c6a2467eea625f2c2266d69598ef073ea
mar3hc <tri.maiminh@vn.bosch.com>
2025-02-10 13:59:57 +0700

    Update history

--- RobotFramework_UDS/version.py
+++ RobotFramework_UDS/version.py
@@ -12,5 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-VERSION      = '0.1.11'
+VERSION      = '0.1.12'
 VERSION_DATE = '10.02.2024'
